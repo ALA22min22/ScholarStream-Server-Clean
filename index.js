036c3fdec2963aab0d31a8336a98b9538a3b0e29
alamin22 <alaminhossan4000@gmail.com>
2025-12-23 17:27:57 +0600

    some chenges for vercel deploy and and it is worked

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        await client.connect();
+        // await client.connect();
 
         //collections:
         const myDB = client.db("ScholarStream");
@@ -546,7 +546,7 @@ async function run() {
 
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
